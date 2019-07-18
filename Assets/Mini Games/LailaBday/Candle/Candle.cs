7c74add87214264b5074081dd9f6bed550ef5cf9
Rhiannan Berry <rhiannanb97@gmail.com>
2019-07-17 21:02:46 -0400

    Laila last minute bday presentgit statusgit status!

---
+++ Assets/Mini Games/LailaBday/Candle/Candle.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Candle : MonoBehaviour
+{
+    public static int numCandles = 0;
+    public Sprite outCandleSprite;
+    private SpriteRenderer sr;
+
+    void Start()
+    {
+        numCandles++;
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void PutOut() {
+        GetComponent<Animator>().enabled = false;
+        sr.sprite = outCandleSprite;
+        numCandles--;
+        CheckWin();
+    }
+
+    public void OnMouseDown() {
+        if (GetComponent<Animator>().enabled)
+            PutOut();
+    }
+
+    private void CheckWin() {
+        if (numCandles == 0)
+            EventManager.TriggerEvent("Game Win");
+    }
+}
